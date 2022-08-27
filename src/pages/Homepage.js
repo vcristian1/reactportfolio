72555e574d0b974f1121e1fcf5b6ad4f75e12cdb
Cristian Vargas <cristian.v0223@gmail.com>
2022-08-27 11:53:04 -0500

    downloaded react-router-dom

---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AboutCard from "../components/AboutCard";
+
+function Homepage() {
+    
+    return (
+        <AboutCard></AboutCard>
+    );
+}
+
+export default Homepage;
