516644e51a5d445e8297bc0e10bdd86358daf952
Cristian Vargas <cristian.v0223@gmail.com>
2022-08-26 21:40:19 -0500

    added aboutcard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from './components/Navbar'
+import AboutCard from "./components/AboutCard";
 
 function App() {
   return (
     <div className="App">
       < Navbar></Navbar>
+      < AboutCard></AboutCard>
     </div>
   );
 }
