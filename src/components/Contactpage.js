444c7778b1260403c87a4c52fcb3976235ab4d1d
Cristian Vargas <cristian.v0223@gmail.com>
2022-09-20 20:49:04 -0500

    removed duplicate footer in contact page component

--- src/components/Contactpage.js
+++ src/components/Contactpage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ContactCard from './ContactCard'
-import Footer from './Footer'
 import Layout from './layout'
 
 function Contactpage(){
     return(
         <Layout>
             <ContactCard />
-            <Footer />
         </ Layout>
     )
 }
